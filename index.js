d7be5f51e70068458043328bcbaf894e6d1b3b21
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-08-17 08:40:19 +0700

    fix api error

--- index.js
+++ index.js
@@ -12,28 +12,11 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 app.use('/', express.static(path.join(__dirname, 'build')));
 
-// app.use(function (req, res, next) {
-//    res.setHeader('Access-Control-Allow-Origin', '*');
-//    res.setHeader(
-//       'Access-Control-Allow-Methods',
-//       'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-//    );
-//    res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-Requested-With,content-type'
-//    );
-//    res.setHeader('Access-Control-Allow-Credentials', true);
-
-//    next();
-// });
-
 const PORT = process.env.PORT || 4000;
 
 //Routers
 route(app);
 
 app.listen(PORT, () => {
-   console.log(
-      chalk.bold.cyan(`server is running at http://dungxbuif-localhost:${PORT}`)
-   );
+   console.log(chalk.bold.cyan(`server is running at http://localhost:${PORT}`));
 });
